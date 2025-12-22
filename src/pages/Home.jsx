64532cd65396b27091350059a26db7e72b34ce5d
Frederik Bjørn <fbjoern18@gmail.com>
2025-12-22 11:13:23 +0100

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,37 @@ export default function Home() {
     <main
       style={{
         minHeight: "100vh",
-        display: "grid",
-        placeItems: "center",
-        fontFamily: "system-ui, sans-serif",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 12,
         textAlign: "center",
       }}
     >
-      <h1>Website under construction… </h1>
+      <h1 style={{ marginBottom: 8 }}>
+        Designing a Dynamic Music System for Narrative Audio Storytellers
+      </h1>
+
+      <h2 style={{ marginTop: 0, marginBottom: 16 }}>
+        An HCI design approach to Creative Audio Software
+      </h2>
+
+      <ul
+        style={{
+          marginTop: 0,
+          paddingLeft: 0,
+          listStyle: "none",
+          textAlign: "left",
+        }}
+      >
+        <li>Frederik Bjørn Blauenfeldt</li>
+        <li>fbbl@itu.dk</li>
+        <li>Software Design Thesis</li>
+        <li>KISPECI1SE</li>
+        <li>Supervisor – Louise Barkhuus</li>
+        <li>A collaboration study with Hindenburg Systems</li>
+      </ul>
     </main>
   );
 }
